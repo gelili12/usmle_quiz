051c5db3611ae0a43f8e256d972595c8e03331af
gelili12 <129226771+gelili12@users.noreply.github.com>
2023-03-28 23:28:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 let data = [];
+let currentQuestion = 0;
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
 
 async function loadData() {
     const response = await fetch("questions.jsonl");
@@ -9,24 +17,22 @@ async function loadData() {
         data.push(JSON.parse(line));
     }
 
+    shuffleArray(data);
     displayQuestion();
 }
 
-let currentQuestion = 0;
-
 function displayQuestion() {
-    const question = document.getElementById("question");
-    const optionA = document.getElementById("A");
-    const optionB = document.getElementById("B");
-    const optionC = document.getElementById("C");
-    const optionD = document.getElementById("D");
+    const questionData = data[currentQuestion];
+    document.getElementById("question").textContent = questionData.question;
 
-    question.textContent = data[currentQuestion].question;
-    optionA.textContent = data[currentQuestion].options.A;
-    optionB.textContent = data[currentQuestion].options.B;
-    optionC.textContent = data[currentQuestion].options.C;
-    optionD.textContent = data[currentQuestion].options.D;
+    let optionsHtml = "";
+    for (const key in questionData.options) {
+        optionsHtml += `<button onclick="checkAnswer('${key}')" class="option">${key}: ${questionData.options[key]}</button>`;
+    }
+
+    document.getElementById("options").innerHTML = optionsHtml;
 }
+
 function checkAnswer(selectedOption) {
     const questionData = data[currentQuestion];
     const correctOption = questionData.answer_idx;
@@ -40,39 +46,6 @@ function checkAnswer(selectedOption) {
 
     document.getElementById("answer").textContent = answerText;
     document.getElementById("next").style.display = "block";
-
-    // Disable the option buttons after checking the answer
-    const optionButtons = document.getElementsByClassName("option");
-    for (let i = 0; i < optionButtons.length; i++) {
-        optionButtons[i].disabled = true;
-    }
-}
-
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-const numberOfQuestionsToLoad = 20;
-
-async function loadData() {
-    const response = await fetch("questions.jsonl");
-    const text = await response.text();
-    const lines = text.split("\n").filter(line => line.trim() !== "");
-
-    const allData = [];
-    for (const line of lines) {
-        allData.push(JSON.parse(line));
-    }
-
-    shuffleArray(allData);
-
-    for (let i = 0; i < numberOfQuestionsToLoad; i++) {
-        data.push(allData[i]);
-    }
-
-    displayQuestion();
 }
 
 function nextQuestion() {
@@ -86,11 +59,6 @@ function nextQuestion() {
         document.getElementById("answer").textContent = "No more questions!";
         document.getElementById("next").style.display = "none";
     }
-}
-
-function checkAnswer(option) {
-    // ... (existing checkAnswer() function content) ...
-    document.getElementById("next").style.display = "block";
 }
 
 loadData();
