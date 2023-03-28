0d1f5607daa2b325175fd2eb0e2419d3c0832493
gelili12 <129226771+gelili12@users.noreply.github.com>
2023-03-28 23:50:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 let data = [];
+let currentQuestion = 0;
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
 
 async function loadData() {
     const response = await fetch("questions.jsonl");
@@ -9,63 +17,52 @@ async function loadData() {
         data.push(JSON.parse(line));
     }
 
-    displayQuestion();
-}
-
-let currentQuestion = 0;
-
-function displayQuestion() {
-    const question = document.getElementById("question");
-    const optionA = document.getElementById("A");
-    const optionB = document.getElementById("B");
-    const optionC = document.getElementById("C");
-    const optionD = document.getElementById("D");
-
-    question.textContent = data[currentQuestion].question;
-    optionA.textContent = data[currentQuestion].options.A;
-    optionB.textContent = data[currentQuestion].options.B;
-    optionC.textContent = data[currentQuestion].options.C;
-    optionD.textContent = data[currentQuestion].options.D;
-}
-
-function checkAnswer(option) {
-    const answer = document.getElementById("answer");
-    if (option === data[currentQuestion].answer_idx) {
-        answer.textContent = "Correct! The answer is " + data[currentQuestion].answer;
-    } else {
-        answer.textContent = "Incorrect! The correct answer is " + data[currentQuestion].answer;
-    }
-}
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
-async function loadData() {
-    // ... (existing loadData() function content) ...
     shuffleArray(data);
     displayQuestion();
 }
 
+function displayQuestion() {
+    const questionData = data[currentQuestion];
+    document.getElementById("question").textContent = questionData.question;
+
+    let optionsHtml = "";
+    for (const key in questionData.options) {
+        optionsHtml += `<button onclick="checkAnswer('${key}')" class="option">${key}: ${questionData.options[key]}</button>`;
+    }
+
+    document.getElementById("options").innerHTML = optionsHtml;
+}
+
+function checkAnswer(selectedOption) {
+    const questionData = data[currentQuestion];
+    const correctOption = questionData.answer_idx;
+
+    let answerText = "";
+        if (selectedOption === correctOption) {
+        answerText = "Correct!";
+    } else {
+        answerText = `Incorrect. The correct answer is ${correctOption}: ${questionData.options[correctOption]}.`;
+    }
+
+    document.getElementById("answer").textContent = answerText;
+    document.getElementById("next").style.display = "inline";
+}
+
 function nextQuestion() {
-    if (currentQuestion < data.length - 1) {
-        currentQuestion++;
+    currentQuestion++;
+
+    if (currentQuestion < data.length) {
         displayQuestion();
-        const answer = document.getElementById("answer");
-        answer.textContent = "";
+        document.getElementById("answer").textContent = "";
         document.getElementById("next").style.display = "none";
     } else {
-        document.getElementById("answer").textContent = "No more questions!";
+        document.getElementById("question").textContent = "No more questions.";
+        document.getElementById("options").innerHTML = "";
+        document.getElementById("answer").textContent = "";
         document.getElementById("next").style.display = "none";
     }
 }
 
-function checkAnswer(option) {
-    // ... (existing checkAnswer() function content) ...
-    document.getElementById("next").style.display = "block";
-}
-
-
 loadData();
+
+       
